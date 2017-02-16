695a9a8543ba4880ca5edfdf7bc0f55d3701b005
abhishek dhabriya <abhishek.dhabriya@gmail.com>
2017-02-15 16:28:16 -0800

    Initial checkin with webpack

---
+++ src/client/components/app.js
@@ -0,0 +1,32 @@
+import './app.scss';
+
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this._click = this._click.bind(this);
+        
+    }
+
+    componentDidMount() {
+        console.log('HEY you!');
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hey webpack! </h1>
+                <button onClick={this._click}>Click again </button>
+            </div>
+        );
+    }
+
+    _click() {
+        console.log('this will');
+    }
+}
+
+export default AppContainer;
